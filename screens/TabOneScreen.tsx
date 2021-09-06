874b7ebfb5725f3af92797f0cf94d2e748cdbba6
Muhammad Syawaludin <syawalhastaf@gmail.com>
2021-09-06 18:55:03 +0800

    initial sudoku box commit

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,46 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Dimensions } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
+const windowWidth = Dimensions.get('window').width;
+
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+
+  const [boardData, setBoardData] = useState([]);
+  const fetchGame = () => {
+    fetch('https://sugoku.herokuapp.com/board?difficulty=easy')
+      .then(response => response.json())
+      .then(data => setBoardData(data.board));
+  }
+
+  useEffect(() => {
+    fetchGame();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <View style={styles.boxContainer}>
+        {boardData?.map((obj: any, index: number) => {
+          return (
+            <View
+              key={index}
+              style={styles.itemContainer}
+            >
+              {obj.map((number: any, i: number) => {
+                return (
+                  <View
+                    key={index + i + 'num'}
+                    style={styles.itemNumber}
+                  >
+                    <Text>{number == 0 ? '' : number }</Text>
+                  </View>
+                )
+              })}
+            </View>
+          )
+        })}
+      </View>
     </View>
   );
 }
@@ -21,13 +51,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  boxContainer: {
+    height: windowWidth,
+    width: windowWidth,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  itemContainer: {
+    borderWidth: 1,
+    borderColor: 'green',
+    height: '33%',
+    width: '33%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
+  itemNumber: {
+    borderWidth: 1,
+    borderColor: 'red',
+    height: '33%',
+    width: '33%',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 });
